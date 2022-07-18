ef159a8e9afbddab82fee5d5d827bc18f5c4d996
Mohammed Afzaal <mohdafzaal71@gmail.com>
2022-07-18 17:16:12 +0300

    End of Section 8

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+using System.Linq;
+
+namespace API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AppUser, MemberDto>()
+                .ForMember(destination => destination.PhotoUrl,
+                            options => options.MapFrom(source => source.Photos.FirstOrDefault(x => x.IsMain).Url)) //I am mapping profile picture from phtodto object
+                .ForMember(destination => destination.Age,
+                            options => options.MapFrom(source => source.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotoDto>();
+        }
+    }
+}
